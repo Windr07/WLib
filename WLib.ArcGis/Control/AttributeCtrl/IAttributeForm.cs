cebde799d78c69e58c599100ec6687f71845744a
windr07 <366146+windr07@user.noreply.gitee.com>
2020-03-06 13:05:01 +0800

    添加或调整ArcGIS Engine相关的代码

---
+++ WLib.ArcGis/Control/AttributeCtrl/IAttributeForm.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows.Forms;
+
+namespace WLib.ArcGis.Control.AttributeCtrl
+{
+    /// <summary>
+    /// 显示属性表的窗体
+    /// </summary>
+    public interface IAttributeForm
+    {
+        IAttributeCtrl AttributeCtrl { get; }
+
+
+        #region 控件本身的属性和方法
+        /// <summary>
+        /// 
+        /// </summary>
+        bool IsDisposed { get; }
+        /// <summary>
+        /// 
+        /// </summary>
+        bool Visible { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        void Activate();
+        /// <summary>
+        /// 
+        /// </summary>
+        void Show(IWin32Window win32Window);
+        /// <summary>
+        /// 
+        /// </summary>
+        event FormClosingEventHandler FormClosing;
+        #endregion
+    }
+}
