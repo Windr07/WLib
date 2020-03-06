c73e16739295deea355b507417d588affb094086
windr07 <366146+windr07@user.noreply.gitee.com>
2020-03-06 12:49:55 +0800

    添加新的路径选择控件

---
+++ WLib.WinCtrls.Dev/PathCtrl/LabelPathBoxControl.cs
@@ -0,0 +1,38 @@
+﻿namespace WLib.WinCtrls.Dev.PathCtrl
+{
+    /// <summary>
+    /// 在路径选择与显示的组合框<see cref="PathBoxControl"/>基础上，加上路径标题的控件
+    /// </summary>
+    public partial class LabelPathBoxControl : DevExpress.XtraEditors.XtraUserControl
+    {
+        /// <summary>
+        /// 标题
+        /// </summary>
+        public override string Text { get => this.TitleLabel.Text; set => this.TitleLabel.Text = value; }
+        /// <summary>
+        /// 路径
+        /// </summary>
+        public string Path { get => this.PathBox.Path; set => this.PathBox.Path = value; }
+
+
+        /// <summary>
+        /// 在路径选择与显示的组合框<see cref="PathBoxControl"/>基础上，加上路径标题的控件
+        /// </summary>
+        public LabelPathBoxControl()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// 在路径选择与显示的组合框<see cref="PathBoxControl"/>基础上，加上路径标题的控件
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="path"></param>
+        public LabelPathBoxControl(string text, string path)
+        {
+            Text = text;
+            Path = path;
+        }
+
+        public override string ToString() => $"{Name} - {Text}: {Path}";
+    }
+}
