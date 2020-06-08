b77c714cc12094ed96194f563ef4b5a0296306cf
windr07 <366146+windr07@user.noreply.gitee.com>
2020-06-08 17:13:28 +0800

    完善代码

--- WLib.Samples.WinForm.Dev/MainForm.Designer.cs
+++ WLib.Samples.WinForm.Dev/MainForm.Designer.cs
@@ -3,14 +3,14 @@
     partial class MainForm
     {
         /// <summary>
-        /// 必需的设计器变量。
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// 清理所有正在使用的资源。
+        /// Clean up any resources being used.
         /// </summary>
-        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,85 +20,20 @@
             base.Dispose(disposing);
         }
 
-        #region Windows 窗体设计器生成的代码
+        #region Windows Form Designer generated code
 
         /// <summary>
-        /// 设计器支持所需的方法 - 不要修改
-        /// 使用代码编辑器修改此方法的内容。
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
-            this.btnExport = new DevExpress.XtraEditors.SimpleButton();
-            this.imageCollection_Nav = new DevExpress.Utils.ImageCollection(this.components);
-            this.btnRunIntersect = new DevExpress.XtraEditors.SimpleButton();
-            ((System.ComponentModel.ISupportInitialize)(this.imageCollection_Nav)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // btnExport
-            // 
-            this.btnExport.Location = new System.Drawing.Point(93, 107);
-            this.btnExport.Name = "btnExport";
-            this.btnExport.Size = new System.Drawing.Size(133, 41);
-            this.btnExport.TabIndex = 0;
-            this.btnExport.Text = "导出";
-            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
-            // 
-            // imageCollection_Nav
-            // 
-            this.imageCollection_Nav.ImageStream = ((DevExpress.Utils.ImageCollectionStreamer)(resources.GetObject("imageCollection_Nav.ImageStream")));
-            this.imageCollection_Nav.Images.SetKeyName(0, "Nav_Area_16.png");
-            this.imageCollection_Nav.Images.SetKeyName(1, "Nav_Identify_16.png");
-            this.imageCollection_Nav.Images.SetKeyName(2, "Nav_Identify_32.png");
-            this.imageCollection_Nav.Images.SetKeyName(3, "Nav_Left_16.png");
-            this.imageCollection_Nav.Images.SetKeyName(4, "Nav_Left_32.png");
-            this.imageCollection_Nav.Images.SetKeyName(5, "Nav_Length_16.png");
-            this.imageCollection_Nav.Images.SetKeyName(6, "Nav_Pan_16.png");
-            this.imageCollection_Nav.Images.SetKeyName(7, "Nav_Pan_32.png");
-            this.imageCollection_Nav.Images.SetKeyName(8, "Nav_Right_16.png");
-            this.imageCollection_Nav.Images.SetKeyName(9, "Nav_Right_32.png");
-            this.imageCollection_Nav.Images.SetKeyName(10, "Nav_ZoomFull_16.png");
-            this.imageCollection_Nav.Images.SetKeyName(11, "Nav_ZoomFull_32.png");
-            this.imageCollection_Nav.Images.SetKeyName(12, "Nav_ZoomIn_16.png");
-            this.imageCollection_Nav.Images.SetKeyName(13, "Nav_ZoomIn_32.png");
-            this.imageCollection_Nav.Images.SetKeyName(14, "Nav_ZoomOut_16.png");
-            this.imageCollection_Nav.Images.SetKeyName(15, "Nav_ZoomOut_32.png");
-            this.imageCollection_Nav.Images.SetKeyName(16, "Nav_Close_16.png");
-            this.imageCollection_Nav.Images.SetKeyName(17, "table2.png");
-            this.imageCollection_Nav.Images.SetKeyName(18, "EffectsSwipe16.png");
-            this.imageCollection_Nav.Images.SetKeyName(19, "EffectsSwipe32.png");
-            this.imageCollection_Nav.Images.SetKeyName(20, "SelectionSelectPolygonTool16.png");
-            this.imageCollection_Nav.Images.SetKeyName(21, "SelectionSelectTool16.png");
-            // 
-            // btnRunIntersect
-            // 
-            this.btnRunIntersect.Location = new System.Drawing.Point(265, 107);
-            this.btnRunIntersect.Name = "btnRunIntersect";
-            this.btnRunIntersect.Size = new System.Drawing.Size(133, 41);
-            this.btnRunIntersect.TabIndex = 0;
-            this.btnRunIntersect.Text = "运行相交工具";
-            this.btnRunIntersect.Click += new System.EventHandler(this.btnRunIntersect_Click);
-            // 
-            // MainForm
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(933, 525);
-            this.Controls.Add(this.btnRunIntersect);
-            this.Controls.Add(this.btnExport);
-            this.Name = "MainForm";
-            this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.imageCollection_Nav)).EndInit();
-            this.ResumeLayout(false);
-
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "MainForm";
         }
 
         #endregion
-
-        private DevExpress.XtraEditors.SimpleButton btnExport;
-        private DevExpress.Utils.ImageCollection imageCollection_Nav;
-        private DevExpress.XtraEditors.SimpleButton btnRunIntersect;
     }
 }
-
--- WLib.Samples.WinForm.Dev/MainForm.cs
+++ WLib.Samples.WinForm.Dev/MainForm.cs
@@ -1,44 +1,12 @@
-﻿using System;
-using System.Windows.Forms;
-using WLib.ArcGis.Analysis.Gp;
-using WLib.WinCtrls.Dev.Extension;
+﻿using System.Windows.Forms;
 
 namespace WLib.Samples.WinForm.Dev
 {
-    public partial class MainForm : DevExpress.XtraEditors.XtraForm
+    public partial class MainForm : Form
     {
         public MainForm()
         {
             InitializeComponent();
-        }
-
-        private void btnExport_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                imageCollection_Nav.ExportImageCollectionImages(Environment.CurrentDirectory + @"\Photo\");
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(@"错误：", ex.Message, MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-        }
-
-        private void btnRunIntersect_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                var dbPath = @"F:\工作事项\11、耕地质量系列项目\测试数据\440111白云区江高镇雄丰村、五丰村、中八村2014年高标准基本农田建设项目DBSJK.gdb";
-                var path1 = dbPath + @"\XMGXDY;";
-                var path2 = dbPath + @"\ZDZWZJCGSXB";
-                var path3 = dbPath + @"\JZZWZJCGSXB";
-                var resultPath = @"";
-                new GpHelper(true).RunTool(GpHelper.Intersect(path1, resultPath), out _, out _);
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(@"错误：", ex.Message, MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
         }
     }
 }
